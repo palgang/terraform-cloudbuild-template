c2e19bdc3d5f091df49764b1b21f85af788ed224
palgang <palgangmori@gmail.com>
2020-10-24 16:17:06 +0200

    Create variables.tf

---
+++ environments/dev/variables.tf
@@ -0,0 +1,6 @@
+# Each input variable must be declared using a variable block
+
+variable "project" {
+  type="string"
+  description = "The id of the project for which GitOps and Infrastructure as the Code is set up"
+}
