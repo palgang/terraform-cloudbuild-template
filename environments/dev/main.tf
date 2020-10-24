53d210f9df8e3a5a4c3caf01f90b1c23cada8fe6
palgang <palgangmori@gmail.com>
2020-10-24 22:05:39 +0200

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -5,3 +5,14 @@ locals {
 provider "google" {
   project = "${var.project}"
 }
+module "vpc" {
+  source  = "../../modules/vpc"
+  project = "${var.project}"
+  env     = "${local.env}"
+}
+
+module "firewall" {
+  source  = "../../modules/firewall"
+  project = "${var.project}"
+  subnet  = "${module.vpc.subnet}"
+}
